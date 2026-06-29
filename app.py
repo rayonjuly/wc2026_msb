cc57c45ed621803c8efdb4ac882e4fa3fb7adda0
rayonjuly <taotaikhoan.127@gmail.com>
2026-06-29 18:55:32 +0700

    first upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="WC 2026 Predictor", layout="wide")
+st.title("🏆 World Cup 2026 Predictor")
+
+tab_leaderboard, tab_bracket = st.tabs(["📊 Leaderboard", "🏟️ Bracket"])
+
+with tab_leaderboard:
+    # st.subheader("Bảng xếp hạng chính thức tính đến 10h00 ngày 2026-06-25")
+    
+    # Load the full dataset from your CSV file
+    # Make sure 'leaderboard_data.csv' is saved in the same folder as this Python script
+
+    df = pd.read_csv(r"standing.csv")
+    
+    # Display the entire dataframe
+    # use_container_width=True makes it stretch across the screen
+    st.dataframe(df, use_container_width=True, hide_index=True)
+        
+    
+    # st.caption("Lưu ý: Việc xếp hạng mới tính trên tổng điểm, chưa tính đến các chỉ số phụ. Có thể thay đổi phụ thuộc vào d/s các đội vào vòng 32_team")
+
+with tab_bracket:
+    st.subheader("Tournament Bracket")
+    st.info("Your bracket code goes here.")
