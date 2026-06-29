6e15ecfda7e61b77be1bbe418326a87cf5514583
MINHTL3 <taoleminh127@gmail.com>
2026-06-29 22:30:17 +0700

    update

--- app.py
+++ app.py
@@ -12,11 +12,30 @@ with tab_leaderboard:
     # Load the full dataset from your CSV file
     # Make sure 'leaderboard_data.csv' is saved in the same folder as this Python script
 
+    df_dim_team = pd.read_csv('dim_team.csv')
+    dim_team = df_dim_team['team'].tolist()
+
+    dropdown_options = ['All Teams'] + dim_team
+    selected_team = st.selectbox('Filter by Team:', dropdown_options)
+
     df = pd.read_csv(r"standing.csv")
+
+    if selected_team != "All Teams":
+        # Create a filter mask that checks if the selected team is in ANY of the 4 columns
+        mask = (df['team_1'] == selected_team) | \
+                (df['team_2'] == selected_team) | \
+                (df['team_3'] == selected_team) | \
+                (df['team_4'] == selected_team)
+        
+        # Apply the mask to get the filtered data
+        display_df = df[mask]
+    else:
+        # If "All Teams" is selected, show everything
+        display_df = df
     
     # Display the entire dataframe
     # use_container_width=True makes it stretch across the screen
-    st.dataframe(df, use_container_width=True, hide_index=True, height=1000)
+    st.dataframe(display_df, use_container_width=True, hide_index=True, height=1000)
         
     
     # st.caption("Lưu ý: Việc xếp hạng mới tính trên tổng điểm, chưa tính đến các chỉ số phụ. Có thể thay đổi phụ thuộc vào d/s các đội vào vòng 32_team")
