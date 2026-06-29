e1f18b5483bc8fa7629cd54060d74f51bd831e08
MINHTL3 <taoleminh127@gmail.com>
2026-06-29 23:16:05 +0700

    update

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.title("🏆 Bảng xếp hạng dự đoán WC 2026 - MSB")
 # tab_leaderboard = st.tabs(["📊 Bảng xếp hạng"])
 
 # with tab_leaderboard:
-    # st.subheader("Bảng xếp hạng chính thức tính đến 10h00 ngày 2026-06-25")
+st.caption("Cập nhật đến sau trận đấu Canana - Nam Phi")
     
     # Load the full dataset from your CSV file
     # Make sure 'leaderboard_data.csv' is saved in the same folder as this Python script
