653765c0d25dc28b5f7b4a0cec970bd4894d6a8f
Minh <taoleminh127@gmail.com>
2026-07-10 14:10:53 +0700

    update

--- app.py
+++ app.py
@@ -3,29 +3,36 @@ import streamlit.components.v1 as components
 import pandas as pd
 from unidecode import unidecode
 
+
+
 from tabs.tab_leaderboard import render_leaderboard
 from tabs.tab_rules import render_rules
 from tabs.tab_bracket import render_bracket
 from tabs.tab_discussion import render_discussion
 
-st.set_page_config(page_title="Bảng xếp hạng dự đoán WC 2026 - MSB", layout="wide")
-st.title("🏆 Bảng xếp hạng dự đoán WC 2026 - MSB")
-st.caption("Cập nhật đến sau trận đấu France - Morocco")
+import streamlit_analytics2 as streamlit_analytics
 
-tab_leaderboard, tab_score_rules, tab_rules, tab_bracket, tab_discussion = st.tabs(["Bảng xếp hạng", "Cách tính điểm", "Luật", "Phân nhánh", "Tạp chí"])
+# Wrap your entire app logic inside this context manager
+with streamlit_analytics.track():
 
-with tab_leaderboard:
-    render_leaderboard()
+    st.set_page_config(page_title="Bảng xếp hạng dự đoán WC 2026 - MSB", layout="wide")
+    st.title("🏆 Bảng xếp hạng dự đoán WC 2026 - MSB")
+    st.caption("Cập nhật đến sau trận đấu France - Morocco")
 
-with tab_score_rules:
-    st.image("Scoring_rule.png")
+    tab_leaderboard, tab_score_rules, tab_rules, tab_bracket, tab_discussion = st.tabs(["Bảng xếp hạng", "Cách tính điểm", "Luật", "Phân nhánh", "Tạp chí"])
+
+    with tab_leaderboard:
+        render_leaderboard()
+
+    with tab_score_rules:
+        st.image("Scoring_rule.png")
 
 
-with tab_rules: 
-    render_rules()
+    with tab_rules: 
+        render_rules()
 
-with tab_bracket:
-    render_bracket()
+    with tab_bracket:
+        render_bracket()
 
-with tab_discussion:
-    render_discussion()
+    with tab_discussion:
+        render_discussion()
